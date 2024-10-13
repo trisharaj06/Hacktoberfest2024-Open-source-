53e423366bc2f2df1052844406210f34e822f160
trisharaj06 <109025651+trisharaj06@users.noreply.github.com>
2024-10-13 23:44:31 +0530

    Create Anagram.cpp

---
+++ C++/Anagram.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string a, string b){
+        
+        // Your code here
+        if(a.size()!=b.size()){
+            return false;
+        }
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+       for(int i=0;i<a.size();i++){
+           if(a[i]!=b[i]){
+               return false;
+           }
+       }
+        return true;
+    }
+
+};
